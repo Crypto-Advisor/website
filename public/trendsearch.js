95a7fab1512cf42a14d70a57c414ed7713cd02d4
Crypto-Advisor <40330472+Crypto-Advisor@users.noreply.github.com>
2020-11-16 14:09:51 -0800

    day 3

--- public/trendsearch.js
+++ public/trendsearch.js
@@ -9,7 +9,17 @@ function loadContent(result){
   $("main_container").html(result);
 }
 
+function googleTrend(trend){
+  $("#main_container").html('<script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/2402_RC03/embed_loader.js"></script>');
+  trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":trend,"geo":"US","time":"today 12-m"}],"category":0,"property":""}, {"exploreQuery":"q=twitter&geo=US&date=today 12-m","guestPath":"https://trends.google.com:443/trends/embed/"});
+}
+function searchTerm(){
+  var search = document.getElementById("searchValue");
+  googleTrend(search);
+}
+
 function changeSomeHtml(){
   loadContent("nah");
 }
-$("#search").click(changeSomeHtml);
+
+$("#search").click(searchTerm());
