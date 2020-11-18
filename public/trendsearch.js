ec2c3004fa2e4c78eac4c4bcb9fde6b6637e789d
mnwxy <william.k.tao@gmail.com>
2020-11-18 10:47:22 -0800

    Enabled button to display google trend when clicked.
    The error was in using "searchterm()" instead of "searchTerm". Added lines in function searchTerm() to get the value inside the search bar. Also reformatted js code to be in line with code standards.

--- public/trendsearch.js
+++ public/trendsearch.js
@@ -1,33 +1,51 @@
-
 // $.ajax({url: "/pages/splash.html", success: loadContent});
-function unloadContent(){
+function unloadContent() {
   $("#main_container").html('');
 }
 
-function loadContent(result){
+function loadContent(result) {
   unloadContent();
   $("main_container").html(result);
 }
 
-function requestHomeContent(){
-  $.ajax({url:"index.html", success: loadContent()});
+function requestHomeContent() {
+  $.ajax({
+    url: "index.html",
+    success: loadContent()
+  });
 }
 
-function googleTrend(trend){
-  trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"hello","geo":"US","time":"today 12-m"}],"category":0,"property":""}, {"exploreQuery":"q=twitter&geo=US&date=today 12-m","guestPath":"https://trends.google.com:443/trends/embed/"});
+function googleTrend(trend) {
+  trends.embed.renderExploreWidget("TIMESERIES", {
+    "comparisonItem": [{
+      "keyword": trend,
+      "geo": "US",
+      "time": "today 12-m"
+    }],
+    "category": 0,
+    "property": ""
+  }, {
+    "exploreQuery": "q=twitter&geo=US&date=today 12-m",
+    "guestPath": "https://trends.google.com:443/trends/embed/"
+  });
 }
-function searchTerm(){
-  var search = document.getElementById("searchValue");
+
+function searchTerm() {
+  var searchBar = document.getElementById("searchValue");
+  search = searchBar.value;
   googleTrend(search);
 }
 
-function changeSomeHtml(){
+function changeSomeHtml() {
   loadContent("nah");
 }
 
-$("#search").click(searchTerm());
+$("#search").click(searchTerm);
 
 $("#homebutton").click(requestHomeContent());
-$("#game").click(function(){
-  $.ajax({url:"public/game.html", success:loadContent()});
+$("#game").click(function() {
+  $.ajax({
+    url: "public/game.html",
+    success: loadContent()
+  });
 })
